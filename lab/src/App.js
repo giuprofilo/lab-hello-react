c60a2979404eba44c28d4619b7e98893ef4adf21
giuprofilo <giulliavoloton@gmail.com>
2023-02-01 10:58:11 -0300

    done

---
+++ lab/src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import logo from "./images/ironhack-logo-xs.png";
+import menu from "./images/menu-top-xs.png"; 
+import icon1 from "./images/icon1.png";
+import icon2 from "./images/icon2.png";
+import icon3 from "./images/icon3.png";
+import icon4 from "./images/icon4.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav className="Nav">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={menu} className="Menu-logo" alt="menu" />
+        </nav>
+        <h1>
+          Say hello to<br></br>
+          ReactJS
+        </h1>
+        <p>
+          You will learn how to use<br></br>
+          the monst popular frontend library,<br></br>
+          and become a super Ninja developer.
+        </p>
+        <a
+          className="App-link"
+          href=".#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="botao">Awesome!</button>
+        </a>
+      </header>
+      <div className="space-li">
+        <ul className="li">
+          <li>
+            <img src={icon1} className="icon" alt="icon1" />
+            <h3>Declarative</h3>
+            <p>
+              React makes it<br></br>
+              painless to create<br></br>
+              interactive Uls.
+            </p>
+          </li>
+          <li>
+            <img src={icon2} className="icon" alt="icon2" />
+            <h3>Components</h3>
+            <p>
+              Build encapsulated<br></br>
+              components that<br></br>
+              manage their state.
+            </p>
+          </li>
+          <li>
+            <img src={icon3} className="icon" alt="icon3" />
+            <h3>Single-Way</h3>
+            <p>
+              A set of immutable<br></br>
+              values are passed to<br></br>
+              the component's.
+            </p>
+          </li>
+          <li>
+            <img src={icon4} className="icon" alt="icon4" />
+            <h3>JSX</h3>
+            <p>
+              Statically-typed.<br></br>
+              designed to run on<br></br>
+              modern browsers.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
